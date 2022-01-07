10fd041f34a7b00dda09461fe9d0a03878470488
cubsr <levi@blessedmuch.com>
2022-01-07 00:25:04 -0600

    Mostly Tested Vesion No Mail Tests

---
+++ Email Project/Email.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Email_Project
+{
+    public class Email
+    {
+        //holds the log file path
+        private String logFile;
+
+
+
+        //default constructor for no file input
+        public Email()
+        {
+
+            string EmailLog = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "EmailLog.txt");
+            if (!File.Exists(EmailLog))
+            {
+                File.Create(EmailLog);
+                Console.WriteLine("new file created");
+            }
+            logFile = EmailLog;
+        }
+
+        //overloaded constructor if file is given
+        public Email(String fileName)
+        {
+
+            if (!File.Exists(fileName))
+            {
+                File.Create(fileName);
+            }
+            logFile = fileName;
+
+        }
+
+        public async void Sendemail(String sender, String recipient, String subject, String body)
+        {
+            Boolean sent = false;
+            ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap { ExeConfigFilename = "Email Project.exe.config" };
+            Configuration config = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    
+                       var smtpClient = new SmtpClient(config.AppSettings.Settings["Mailserver"].Value)
+                    {
+                        Port = Int32.Parse(config.AppSettings.Settings["Port"].Value),
+                        Credentials = new NetworkCredential(sender, config.AppSettings.Settings["Password"].Value),
+                        EnableSsl = true,
+                    };
+                    
+                    smtpClient.Send("email", "recipient", "subject", "body");
+                    sent = true;
+                    i = 3;
+                    
+
+                } catch(Exception e)
+                {
+                    //Console.WriteLine(e);
+                }
+            }
+            //"using" for memory management
+            using (StreamWriter logger = new StreamWriter(logFile, true))
+            {
+                logger.WriteLine("Sender: {0}", sender);
+                logger.WriteLine("Recipient: {0}", recipient);
+                logger.WriteLine("Subject: {0}", subject);
+                if (sent) { logger.WriteLine("Email Status: Sent"); } else { logger.WriteLine("Email Status: Not sent"); }
+                logger.WriteLine("Email contents: {0}", body);
+                logger.WriteLine("");
+                logger.Close();
+            }
+
+        }
+    }
+}
--- Email Project/Program.cs
+++ Email Project/Program.cs
@@ -1,4 +1,23 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Configuration;
+
+
+
+/*
+ * Send Email Method should be in a dll that can be reused throughout different applications and
+entry points.
+• Email sender, recipient, subject, and body (not attachments), and date must be logged/stored
+indefinitely with status of send attempt.
+• If email fails to send it should either be retried until success or a max of 3 times whichever
+comes first, and can be sent in succession or over a period of time.
+• Please store all credentials in an appsettings instead of hardcoded.
+• At minimum that method/dll should be called from a console application.
+• Extra Credit if attached to an API that can be called from Postman.
+• EXTRA Credit if a front end (wpf/asp.net web application/etc…) calls the API to send the email.
+• In any scenario you should be able to take in an input of a recipient email to send a test email.
+*/
 
 namespace Email_Project
 {
@@ -6,7 +25,20 @@ namespace Email_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Email testMail = new Email();
+            String Sender;
+            String Reciever;
+            String Subject;
+            String Body;
+            Console.Write("Enter email Sender: ");
+            Sender = Console.ReadLine();
+            Console.Write("Enter email Reciever: ");
+            Reciever = Console.ReadLine();
+            Console.Write("Enter email Subject: ");
+            Subject = Console.ReadLine();
+            Console.Write("Enter email Body: ");
+            Body = Console.ReadLine();
+            testMail.Sendemail(Sender, Reciever, Subject, Body);
         }
     }
 }
