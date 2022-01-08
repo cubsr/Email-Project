a1cdb74d40aa6b2e4022ea3d2b30587f2ea4a82b
cubsr <levi@blessedmuch.com>
2022-01-07 23:36:02 -0600

    All points met

--- Email Project/Email.cs
+++ Email Project/Email.cs
@@ -6,18 +6,13 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Net.Mail;
 using System.Configuration;
-using System.Data.SqlClient;
+
 
 namespace Email_Project
 {
     public class Email
     {
-        //holds the log file path
-        private String logFile;
 
-
-
-        //default constructor for no file input
         public Email()
         {
 
@@ -27,51 +22,51 @@ namespace Email_Project
                 File.Create(EmailLog);
                 Console.WriteLine("new file created");
             }
-            logFile = EmailLog;
         }
 
-        //overloaded constructor if file is given
-        public Email(String fileName)
-        {
 
-            if (!File.Exists(fileName))
-            {
-                File.Create(fileName);
-            }
-            logFile = fileName;
-
-        }
-
-        public async void Sendemail(String sender, String recipient, String subject, String body)
+        public static async Task Sendemail(String sender, String recipient, String subject, String body)
         {
             Boolean sent = false;
             ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap { ExeConfigFilename = "Email Project.exe.config" };
             Configuration config = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+
             for (int i = 0; i < 3; i++)
             {
                 try
                 {
-                    
-                       var smtpClient = new SmtpClient(config.AppSettings.Settings["Mailserver"].Value)
+                    //setup SMTP setting from appsettings
+                    var smtpClient = new SmtpClient(config.AppSettings.Settings["Mailserver"].Value)
                     {
                         Port = Int32.Parse(config.AppSettings.Settings["Port"].Value),
                         Credentials = new NetworkCredential(sender, config.AppSettings.Settings["Password"].Value),
                         EnableSsl = true,
                     };
-                    
-                    smtpClient.Send("email", "recipient", "subject", "body");
+
+                    //send email
+                    smtpClient.Send(sender, recipient, subject, body);
+                    //break from loop
                     sent = true;
                     i = 3;
-                    
 
-                } catch(Exception e)
+
+                }
+                catch (Exception e)
                 {
-                    //Console.WriteLine(e);
+                    Console.WriteLine(e);
                 }
             }
-            //"using" for memory management
-            using (StreamWriter logger = new StreamWriter(logFile, true))
+        
+            //create a file to log email attempts
+            string EmailLog = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "EmailLog.txt");
+            if (!File.Exists(EmailLog))
             {
+                File.Create(EmailLog);
+            }
+            //"using" for memory management
+            using (StreamWriter logger = new StreamWriter(EmailLog, true))
+            {
+                //log sender, recipient, subject, email status, and body of mail message
                 logger.WriteLine("Sender: {0}", sender);
                 logger.WriteLine("Recipient: {0}", recipient);
                 logger.WriteLine("Subject: {0}", subject);
@@ -80,6 +75,8 @@ namespace Email_Project
                 logger.WriteLine("");
                 logger.Close();
             }
+
+            return;
 
         }
     }
